a632cd08889d79a3857645a2a7dee736e52ea470
Max DePriest <maxd924@gmail.com>
2016-07-31 15:24:16 -0500

    removed some headers and enlarged buttons

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -16,6 +16,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to(:users, notice: 'Logged out!')
+    redirect_to('/home', notice: 'Logged out!')
   end
 end
